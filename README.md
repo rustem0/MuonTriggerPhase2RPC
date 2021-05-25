56c132986b3917e3f504eb90975b53c92f1d7608
rustemos <67321992+rustemos@users.noreply.github.com>
2021-05-25 10:09:32 +0200

    add new plots for differences of muon path and hits; clean up code (#1)
    * making code slightly more robust
    
    * add difference plots
    
    * save plots in sub-directory of input data

--- README.md
+++ README.md
@@ -7,13 +7,38 @@ MuonTriggerPhase2RPC: python code for toy simulation of ATLAS RPC trigger
 
 This package contains python macros for toy simulation of the ATLAS RPC trigger and for study of neural network regression for estimating transverse momentum of muon candidates.
 
+Dependencies
+------------
+ 
+These macros require Python 3.7+ and PyTorch
+
 
 Documentation
 -------------
 
-Produce events for NN training without noise:
-
+Make events for NN training without noise:
 ```bash
-python3 macros/runMuonSimulation.py --logy -n 100000 --noise-prob=0.0 -o train_100k_noise-off/train_100k_noise-off.csv &> log100k &
-
+python3 macros/runMuonSimulation.py --logy -n 100000 --noise-prob=0.0 --seed=42
 ```
+
+Run NN training using these events:
+```bash
+python3 macros/trainNN.py rpc-sim_100000-events_0000-noise_000042-seed/events.csv
+```
+
+Make events for NN evaluation with noise:
+```bash
+python3 macros/runMuonSimulation.py --logy -n 200000 --noise-prob=0.001 --seed=101042
+```
+
+Make plots:
+```bash
+python3 macros/runMuonSimulation.py --logy --in-pickle=rpc-sim_200000-events_0010-noise_101042-seed/events.pickle --torch-model=rpc-sim_100000-events_0000-noise_000042-seed/events.pt --plot
+```
+
+Draw event displays:
+```bash
+python3 macros/runMuonSimulation.py --logy --in-pickle=rpc-sim_200000-events_0010-noise_101042-seed/events.pickle --torch-model=rpc-sim_100000-events_0000-noise_000042-seed/events.pt --draw
+```
+
+
--- macros/trainNN.py
+++ macros/trainNN.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import sys
 import pprint
+import logging
 
 import torch
 import torch.nn as nn
@@ -29,13 +30,104 @@ p.add_option('--mse',             action = 'store_true', default = False, help='
 p.add_option('--l1e',             action = 'store_true', default = False, help='use linear error loss function')
 p.add_option('--sme',             action = 'store_true', default = False, help='use smooth linear error loss function')
 
-
 p.add_option('-d', '--debug',     action = 'store_true', default = False, help='print debug info')
 p.add_option('-p', '--plot',      action = 'store_true', default = False, help='plot histograms')
 
-
 (options, args) = p.parse_args()
 
+#----------------------------------------------------------------------------------------------    
+def getFilePath(suffix=None):
+
+    if len(args) != 1:
+        info('main - missing input data path: {}'.format(args))
+        return
+
+    fpath = args[0]
+
+    if fpath.count('.csv') != 1:
+        info('main - input file must end with .csv: {}'.format(fpath))
+        sys.exit(0)
+
+    if suffix == None:
+        if not os.path.isfile(fpath):
+            info('main - missing input data path: {}'.format(fpath))
+            sys.exit(0)
+
+        return fpath
+
+    fbase = os.path.dirname(fpath)
+
+    if fbase and len(fbase) > 1 and not os.path.isdir(fbase):
+        raise Exception('getFilePath - invalid directory="{}" - this should never happen'.format(fbase))
+
+    return fpath.replace('.csv', suffix)
+
+#----------------------------------------------------------------------------------------------
+def getLog(name, level='INFO', debug=False, print_time=False, verbose=False):
+
+    if print_time:
+        f = logging.Formatter('%(asctime)s - %(name)s: %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    elif options.debug:
+        f = logging.Formatter('%(name)s: %(levelname)s - %(message)s')
+    else:
+        f = logging.Formatter('%(message)s')
+
+    ff = logging.Formatter('%(asctime)s: %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+    fh = logging.FileHandler(filename=getFilePath('_train.log'))
+    fh.setFormatter(f)
+    fh.setFormatter(ff)
+
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setFormatter(f)
+
+    log = logging.getLogger(name)
+    log.addHandler(fh)
+    log.addHandler(sh)
+
+    if debug:
+        log.setLevel(logging.DEBUG)
+    else:
+        if level == 'DEBUG':   log.setLevel(logging.DEBUG)
+        if level == 'INFO':    log.setLevel(logging.INFO)
+        if level == 'WARNING': log.setLevel(logging.WARNING)
+        if level == 'ERROR':   log.setLevel(logging.ERROR)
+
+    return log
+
+#----------------------------------------------------------------------------------------------
+log = getLog(os.path.basename(__file__), debug=options.debug)
+
+#----------------------------------------------------------------------------------------------
+def info(*args):
+
+    out = ''
+
+    for arg in args:
+        out += '{} '.format(arg)
+
+    log.info(out)
+
+#----------------------------------------------------------------------------------------------    
+def waitForClick(figs = []):
+
+    if not options.plot:
+        return
+
+    info('Click on figure to continue, close to exit programme...')
+
+    while True:
+        try:
+            result = plt.waitforbuttonpress()
+        except:
+            info('waitForClick - exiting programme on canvas close')
+            sys.exit(0)
+
+        if result == False:
+            break
+
+    for f in figs:
+        plt.close(f)
 
 #----------------------------------------------------------------------------------------------    
 def prepareData(datapath):
@@ -45,11 +137,11 @@ def prepareData(datapath):
     trainDr = (event.values[:,0:3])
     trainTr =  event.values[:,3:4]
 
-    print('train data:   mean={}, std={}'.format(trainDr.mean(axis=0), trainDr.std(axis=0)))
-    print('train target: mean={}, std={}'.format(trainTr.mean(axis=0), trainTr.std(axis=0)))
+    info('train data:   mean={}, std={}'.format(trainDr.mean(axis=0), trainDr.std(axis=0)))
+    info('train target: mean={}, std={}'.format(trainTr.mean(axis=0), trainTr.std(axis=0)))
 
-    print(trainDr)
-    print(trainTr)
+    info(trainDr)
+    info(trainTr)
 
     trainD = trainDr.astype(np.float32)
     trainT = trainTr.astype(np.float32)
@@ -57,10 +149,10 @@ def prepareData(datapath):
     trainDm = torch.from_numpy(trainD)
     trainTm = torch.from_numpy(trainT)
 
-    print('trainDr shape:', trainDr.shape)
-    print('trainDm shape:', trainDm.shape)
-    print('trainTr shape:', trainTr.shape)
-    print('trainTm shape:', trainTm.shape)
+    info('trainDr shape:', trainDr.shape)
+    info('trainDm shape:', trainDm.shape)
+    info('trainTr shape:', trainTr.shape)
+    info('trainTm shape:', trainTm.shape)
 
     return [trainDm, trainTm]
 
@@ -80,54 +172,6 @@ class Net(nn.Module):
         x = nn.functional.relu(self.fc3(x))
         x = self.fc4(x)
         return x
-
-#----------------------------------------------------------------------------------------------    
-def getOutFile(suffix=None):
-
-    if len(args) != 1:
-        print('main - missing input data path: {}'.format(args))
-        sys.exit(0)
-
-    fpath = args[0]
-
-    if suffix == None:
-        if not os.path.isfile(fpath):
-            print('main - missing input data path: {}'.format(fpath))
-            sys.exit(0)
-
-        return fpath
-
-    if fpath.count('.csv') != 1:
-        print('main - input file must end with .csv: {}'.format(fpath))
-        sys.exit(0)
-
-    fbase = fpath.replace('.csv', '')
-
-    if not os.path.isdir(fbase):
-        os.makedirs(fbase)
-
-    return '{}/{}'.format(fbase, fpath.replace('.csv', suffix))
-
-#----------------------------------------------------------------------------------------------    
-def waitForClick(figs = []):
-
-    if not options.plot:
-        return
-
-    print('Click on figure to continue, close to exit programme...')
-
-    while True:
-        try:
-            result = plt.waitforbuttonpress()
-        except:
-            print('waitForClick - exiting programme on canvas close')
-            sys.exit(0)
-
-        if result == False:
-            break
-
-    for f in figs:
-        plt.close(f)
 
 #----------------------------------------------------------------------------------------------    
 def learnReg(net, nepoch, learningRate, trainDm, trainTm, currStep):
@@ -150,49 +194,45 @@ def learnReg(net, nepoch, learningRate, trainDm, trainTm, currStep):
         loss.backward()
         optimizer.step()
 
-        print('Epoch [{}/{}], Loss: {:.4f}'.format(epo+1, nepoch, loss.data.numpy()))
+        info('Epoch [{}/{}], Loss: {:.4f}'.format(epo+1, nepoch, loss.data.numpy()))
         TotalLoss[epo] = loss.data.numpy()
 
     pred = net(trainDm).detach().numpy()
-    print(pred)
+    info(pred)
 
     f1 = plt.figure()
     plt.scatter(x = trainTm, y = pred, marker = 'o', s = 0.1)
     plt.xlabel('TrainData(1/Gev)')
     plt.ylabel('Prediction(1/Gev)')
     f1.show(False)
-    plt.savefig(getOutFile('_epoch{:d}_TargetVsResult.png'.format(currStep)))
+    plt.savefig(getFilePath('_epoch{:d}_TargetVsResult.png'.format(currStep)))
 
     f2 = plt.figure()
     plt.scatter(x = np.arange(0, options.nepoch), y = TotalLoss, s = 0.1)
     plt.xlabel('Epoch')
     plt.ylabel('Total loss')
     f2.show(False)
-    plt.savefig(getOutFile('_epoch{:d}_LossVsEpoch.png'.format(currStep)))
+    plt.savefig(getFilePath('_epoch{:d}_LossVsEpoch.png'.format(currStep)))
 
     waitForClick([f1, f2])
 
 #----------------------------------------------------------------------------------------------    
 def main():
 
-    print('args=', args)
-    print('options=', options)
+    info('args=', args)
+    info('options=', options)
 
     net = Net()
     net.train()
 
-    if len(args) != 1:
-        print('main - missing input data path: {}'.format(args))
-        return
-
-    fpath = args[0]
+    fpath = getFilePath()
 
     if not os.path.isfile(fpath):
-        print('main - missing input data path: {}'.format(fpath))
+        info('main - missing input data path: {}'.format(fpath))
         return
 
     if fpath.count('.csv') != 1:
-        print('main - input file must end with .csv: {}'.format(fpath))
+        info('main - input file must end with .csv: {}'.format(fpath))
         return
 
     data = prepareData(fpath)
@@ -201,10 +241,10 @@ def main():
     learnReg(net, options.nepoch, options.learning_rate2, data[0], data[1], 2)
     learnReg(net, options.nepoch, options.learning_rate3, data[0], data[1], 3)
 
-    print('Finished training, will save state dictionary:')
-    print(pprint.pformat(net.state_dict()))
+    info('Finished training, will save state dictionary:')
+    info(pprint.pformat(net.state_dict()))
 
-    torch.save(net.state_dict(), getOutFile('.net'))
+    torch.save(net.state_dict(), getFilePath('.pt'))
 
 #------------------------------------------------------------------------------#
 if __name__ == "__main__":
